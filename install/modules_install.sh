483a93ef1e1a8602e8339ab24440980fac0ecf09
Mark Boltengagen <m.boltengagen@gmail.com>
2026-01-28 18:42:47 +0100

    tcl install

--- install/modules_install.sh
+++ install/modules_install.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-VERSION=5.6.1
+TCL_VERSION=8.6.13
+MODULE_VERSION=5.6.1
 
+# Install Tcl dependency
 cd /tmp
-wget https://github.com/cea-hpc/modules/releases/download/v${VERSION}/modules-${VERSION}.tar.gz
-tar xzf modules-${VERSION}.tar.gz
-cd modules-${VERSION}
+wget https://prdownloads.sourceforge.net/tcl/tcl${TCL_VERSION}-src.tar.gz
+tar -xzf tcl${TCL_VERSION}-src.tar.gz
+cd tcl${TCL_VERSION}/unix
+
+# Configure and Install Tcl locally
 ./configure --prefix=$INSTALL_DIR
 make -j$(nproc)
 make install
 
-source $INSTALL_DIR/init/bash
+# Install modules
+cd /tmp
+wget https://github.com/cea-hpc/modules/releases/download/v${MODULE_VERSION}/modules-${MODULE_VERSION}.tar.gz
+tar xzf modules-${MODULE_VERSION}.tar.gz
+cd modules-${MODULE_VERSION}
+./configure --prefix=$INSTALL_DIR
+make -j$(nproc)
+make install
+
