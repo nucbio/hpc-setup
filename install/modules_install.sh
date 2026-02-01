eb08f2aa5323fea9d241cb7ce08be0720955a09e
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-01 13:23:47 +0100

    lmod

--- install/modules_install.sh
+++ install/modules_install.sh
@@ -1,44 +1,75 @@
 #!/bin/bash
+LMOD_VERSION=8.7.52
+LUA_VERSION=5.1.4.9
+LUAROCKS_VERSION=3.11.1
 
-TCL_VERSION=8.6.13
-MODULES_VERSION=5.6.1
-# TRY IF WORKS TO LOAD THE LAST and make directory with version
-#MODULES_VERSION=$(get_latest_github_release "cea-hpc/modules")
-
-# Install Tcl dependency
+# Install Lua dependency
 cd /tmp
-if [ -d "tcl${TCL_VERSION}" ]; then
-  rm -rf "tcl${TCL_VERSION}"
+if [ -d "lua-${LUA_VERSION}" ]; then
+  rm -rf "lua-${LUA_VERSION}"
 fi
-wget https://prdownloads.sourceforge.net/tcl/tcl${TCL_VERSION}-src.tar.gz
-tar -xzf tcl${TCL_VERSION}-src.tar.gz
-cd tcl${TCL_VERSION}/unix
+wget https://sourceforge.net/projects/luabinaries/files/${LUA_VERSION}/Tools%20Executables/lua-${LUA_VERSION}_Linux54_64_lib.tar.gz
+mkdir -p lua-${LUA_VERSION}
+cd lua-${LUA_VERSION}
+tar -xzf ../lua-${LUA_VERSION}_Linux54_64_lib.tar.gz
 
-# Configure and Install Tcl locally
-TCL_INSTALL=$INSTALL_DIR/tcl/tcl-$TCL_VERSION
-mkdir -p $TCL_INSTALL
-./configure --prefix=$TCL_INSTALL
-make -j$(nproc)
+# Install Lua from source for better compatibility
+cd /tmp
+if [ -d "lua-5.1.5" ]; then
+  rm -rf "lua-5.1.5"
+fi
+wget http://www.lua.org/ftp/lua-5.1.5.tar.gz
+tar -xzf lua-5.1.5.tar.gz
+cd lua-5.1.5
+
+LUA_INSTALL=$INSTALL_DIR/lua/lua-5.1.5
+mkdir -p $LUA_INSTALL
+make linux INSTALL_TOP=$LUA_INSTALL
+make install INSTALL_TOP=$LUA_INSTALL
+
+# Install LuaRocks (optional but recommended for Lmod dependencies)
+cd /tmp
+if [ -d "luarocks-${LUAROCKS_VERSION}" ]; then
+  rm -rf "luarocks-${LUAROCKS_VERSION}"
+fi
+wget https://luarocks.org/releases/luarocks-${LUAROCKS_VERSION}.tar.gz
+tar -xzf luarocks-${LUAROCKS_VERSION}.tar.gz
+cd luarocks-${LUAROCKS_VERSION}
+
+LUAROCKS_INSTALL=$INSTALL_DIR/luarocks/luarocks-${LUAROCKS_VERSION}
+mkdir -p $LUAROCKS_INSTALL
+./configure --prefix=$LUAROCKS_INSTALL --with-lua=$LUA_INSTALL
+make
 make install
 
-# Install modules
+# Install required Lua modules for Lmod
+export PATH=$LUAROCKS_INSTALL/bin:$PATH
+luarocks install luaposix
+luarocks install luafilesystem
+
+# Install Lmod
 cd /tmp
-if [ -d "modules-${MODULES_VERSION}" ]; then
-  rm -rf "modules-${MODULES_VERSION}"
+if [ -d "Lmod-${LMOD_VERSION}" ]; then
+  rm -rf "Lmod-${LMOD_VERSION}"
 fi
-wget https://github.com/cea-hpc/modules/releases/download/v${MODULES_VERSION}/modules-${MODULES_VERSION}.tar.gz
-tar xzf modules-${MODULES_VERSION}.tar.gz
-cd modules-${MODULES_VERSION}
-MODULES_INSTALL=$INSTALL_DIR/modules/modules-$MODULES_VERSION
-mkdir -p $MODULES_INSTALL
-#./configure --prefix=$INSTALL_DIR/modules/modules-$MODULES_VERSION
+wget https://github.com/TACC/Lmod/archive/${LMOD_VERSION}.tar.gz -O Lmod-${LMOD_VERSION}.tar.gz
+tar -xzf Lmod-${LMOD_VERSION}.tar.gz
+cd Lmod-${LMOD_VERSION}
+
+LMOD_INSTALL=$INSTALL_DIR/lmod/lmod-${LMOD_VERSION}
+mkdir -p $LMOD_INSTALL
+
 ./configure \
-    --prefix=$MODULES_INSTALL \
-    --with-tcl=$TCL_INSTALL/lib \
-    --with-tclsh=$TCL_INSTALL/bin/tclsh8.6
-make -j$(nproc)
+    --prefix=$LMOD_INSTALL \
+    --with-lua=$LUA_INSTALL/bin/lua \
+    --with-luac=$LUA_INSTALL/bin/luac
+
 make install
 
-# Local modules
-source "$INSTALL_DIR/modules/modules-5.6.1/init/bash"
+# Source Lmod
+source "$LMOD_INSTALL/lmod/${LMOD_VERSION}/init/bash"
 
+# Optional: Add to your shell profile for persistence
+# echo "source $LMOD_INSTALL/lmod/${LMOD_VERSION}/init/bash" >> ~/.bashrc
+
+# use for tests: unset -f module
--- installers/test.sh
+++ installers/test.sh
@@ -1,6 +1,6 @@
 module use $INTALL_DIR/modulefiles
-module load meson/1.8.5
-module load ninja/1.13.1
+module load meson/1.10.1
+module load ninja/1.13.2
 
 module --version
 meson --version
