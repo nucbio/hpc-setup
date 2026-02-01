5230c04edb61c3e2854aa1ba44f9343d3dd65f01
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-01 22:10:03 +0100

    luarock_sources

---
+++ modules/luarock1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+LUAROCKS_VERSION="3.13.1" # Latest stable
+cd "$SOURCE_DIR"
+
+# Download source
+wget https://luarocks.org/releases/luarocks-${LUAROCKS_VERSION}.tar.gz
+tar -xzf luarocks-${LUAROCKS_VERSION}.tar.gz
+cd luarocks-${LUAROCKS_VERSION}
+
+# Configure to point specifically to your local Lua
+./configure \
+    --prefix="$LUA_DIR" \
+    --with-lua="$LUA_DIR" \
+    --with-lua-include="$LUA_DIR/include" \
+    --with-lua-lib="$LUA_DIR/lib"
+
+make
+make install
+
+# Clean up
+cd ..
+rm -rf luarocks-${LUAROCKS_VERSION}
+
+luarocks --local install luasocket
+
+$LUA_DIR/bin/luarocks install luaposix
+$LUA_DIR/bin/luarocks install luafilesystem
