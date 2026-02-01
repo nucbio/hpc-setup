dea124bce0139f310f079914b2907f082b22430b
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-01 13:26:29 +0100

    lmod

--- install/modules_install.sh
+++ install/modules_install.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 LMOD_VERSION=8.7.52
-LUA_VERSION=5.1.4.9
+LUA_VERSION=5.1.5
 LUAROCKS_VERSION=3.11.1
 
-# Install Lua dependency
+# Install Lua from source
 cd /tmp
 if [ -d "lua-${LUA_VERSION}" ]; then
   rm -rf "lua-${LUA_VERSION}"
 fi
-wget https://sourceforge.net/projects/luabinaries/files/${LUA_VERSION}/Tools%20Executables/lua-${LUA_VERSION}_Linux54_64_lib.tar.gz
-mkdir -p lua-${LUA_VERSION}
+wget https://github.com/lua/lua/archive/refs/tags/v${LUA_VERSION}.tar.gz -O lua-${LUA_VERSION}.tar.gz
+tar -xzf lua-${LUA_VERSION}.tar.gz
 cd lua-${LUA_VERSION}
-tar -xzf ../lua-${LUA_VERSION}_Linux54_64_lib.tar.gz
 
-# Install Lua from source for better compatibility
-cd /tmp
-if [ -d "lua-5.1.5" ]; then
-  rm -rf "lua-5.1.5"
-fi
-wget http://www.lua.org/ftp/lua-5.1.5.tar.gz
-tar -xzf lua-5.1.5.tar.gz
-cd lua-5.1.5
-
-LUA_INSTALL=$INSTALL_DIR/lua/lua-5.1.5
+LUA_INSTALL=$INSTALL_DIR/lua/lua-${LUA_VERSION}
 mkdir -p $LUA_INSTALL
 make linux INSTALL_TOP=$LUA_INSTALL
 make install INSTALL_TOP=$LUA_INSTALL
@@ -32,7 +22,7 @@ cd /tmp
 if [ -d "luarocks-${LUAROCKS_VERSION}" ]; then
   rm -rf "luarocks-${LUAROCKS_VERSION}"
 fi
-wget https://luarocks.org/releases/luarocks-${LUAROCKS_VERSION}.tar.gz
+wget https://github.com/luarocks/luarocks/archive/refs/tags/v${LUAROCKS_VERSION}.tar.gz -O luarocks-${LUAROCKS_VERSION}.tar.gz
 tar -xzf luarocks-${LUAROCKS_VERSION}.tar.gz
 cd luarocks-${LUAROCKS_VERSION}
 
