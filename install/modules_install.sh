166e3db88d5e159af250207075ed19cb9c66a818
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-01 06:06:52 +0100

    module auto version

--- install/modules_install.sh
+++ install/modules_install.sh
@@ -3,7 +3,7 @@
 TCL_VERSION=8.6.13
 #MODULE_VERSION=5.6.1
 # TRY IF WORKS TO LOAD THE LAST and make directory with version
-MODULE_VERSION=$(get_latest_github_release "cea-hpc/modules")
+MODULES_VERSION=$(get_latest_github_release "cea-hpc/modules")
 
 # Install Tcl dependency
 cd /tmp
@@ -13,17 +13,17 @@ cd tcl${TCL_VERSION}/unix
 
 # Configure and Install Tcl locally
 mkdir -p $INSTALL_DIR/tcl-$TCL_VERSION
-./configure --prefix=$INSTALL_DIR
+./configure --prefix=$INSTALL_DIR/tcl-$TCL_VERSION
 make -j$(nproc)
 make install
 
 # Install modules
 cd /tmp
 wget https://github.com/cea-hpc/modules/releases/download/v${MODULE_VERSION}/modules-${MODULE_VERSION}.tar.gz
-tar xzf modules-${MODULE_VERSION}.tar.gz
-cd modules-${MODULE_VERSION}
-mkdir $INSTALL_DIR/module-$MODULE_VERSION
-./configure --prefix=$INSTALL_DIR
+tar xzf modules-${MODULES_VERSION}.tar.gz
+cd modules-${MODULES_VERSION}
+mkdir $INSTALL_DIR/modules-$MODULES_VERSION
+./configure --prefix=$INSTALL_DIR/modules-$MODULES_VERSION
 make -j$(nproc)
 make install
 
