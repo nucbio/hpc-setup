0ef7c757fa6f9d871812371f86ab513c0cf51671
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-02 09:56:10 +0100

    cairo version

--- r_stack/cairo.sh
+++ r_stack/cairo.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-LIB_DIR=$INSTALL_DIR/cairo/cairo-1.18.4
+CAIRO_VERSION="1.18.4"
+
+LIB_DIR=$INSTALL_DIR/cairo/cairo-$CAIRO_VERSION
 SRC_DIR=$LIB_DIR/src
 BUILD_DIR=/tmp/cairo-build
 
 # Create source directory and extract
 mkdir -p $SRC_DIR
 cd $LIB_DIR
-wget https://cairographics.org/releases/cairo-1.18.2.tar.xz
-tar -xJf cairo-1.18.4.tar.xz -C ${SRC_DIR} --strip-components=1
+wget https://cairographics.org/releases/cairo-$CAIRO_VERSION.tar.xz
+tar -xJf cairo-$CAIRO_VERSION.tar.xz -C ${SRC_DIR} --strip-components=1
 
 # Clean and create build directory
 rm -rf ${BUILD_DIR}
