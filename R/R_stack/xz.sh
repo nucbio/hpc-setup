e384400575fd91735584616c826954d121f0656e
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-03 14:58:12 +0100

    xz location

--- R/R_stack/xz.sh
+++ R/R_stack/xz.sh
@@ -2,21 +2,20 @@
 TOOL_NAME="xz"
 XZ_VERSION="5.8.2"
 SOURCE_URL="https://github.com/tukaani-project/xz/releases/download/v${XZ_VERSION}/xz-${XZ_VERSION}.tar.xz"
-TARGET_DIR="$INSTALL_DIR/xz/$XZ_VERSION"
+TARGET_DIR="$INSTALL_DIR/xz"
 
+mkdir -p $TARGET_DIR
+cd $TARGET_DIR
 # Download and Unpack
-wget -q "$SOURCE_URL" -O xz.tar.xz
-tar -xJf xz.tar.xz
+wget -q "$SOURCE_URL" -O xz-$XZ_VERSION.tar.xz
+tar -xJf xz-$XZ_VERSION.tar.xz
 cd xz-${XZ_VERSION}
 
 # Configure and Install
-./configure --prefix="$TARGET_DIR"
+./configure --prefix="$TARGET_DIR/$TOOL_NAME-$XZ_VERSION"
 
 make -j$(nproc)
 make install
-
-# Clean up
-cd .. && rm -rf xz-${XZ_VERSION} xz.tar.gz
 
 # Modules
 make_lua_module "xz" "$XZ_VERSION"
