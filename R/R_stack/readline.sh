951b6d7deb23e775240fa31e5eff97d616a6ad09
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-03 14:56:26 +0100

    readline location

--- R/R_stack/readline.sh
+++ R/R_stack/readline.sh
@@ -3,26 +3,25 @@
 TOOL_NAME="readline"
 READLINE_VERSION="8.2"
 SOURCE_URL="https://ftp.gnu.org/pub/gnu/readline/readline-${READLINE_VERSION}.tar.gz"
-TARGET_DIR="$INSTALL_DIR/$TOOL_NAME/$READLINE_VERSION"
+TARGET_DIR="$INSTALL_DIR/$TOOL_NAME"
 NCURSES_DIR="$INSTALL_DIR/ncurses/ncurses-$NCURSES_VERSION"
 
+mkdir -p $TARGET_DIR
+cd $TARGET_DIR
 # Download and Unpack
-wget -q "$SOURCE_URL" -O readline.tar.gz
-tar -xzf readline.tar.gz
+wget -q "$SOURCE_URL" -O readline-$READLINE_VERSION.tar.gz
+tar -xzf readline-$READLINE_VERSION.tar.gz
 cd readline-${READLINE_VERSION}
 
 # Configure and Install
 # We link to ncurses so readline knows how to handle the terminal
-./configure --prefix="$TARGET_DIR" \
+./configure --prefix="$TARGET_DIR/readline-$READLINE_VERSION" \
             --with-curses="$NCURSES_DIR" \
             CPPFLAGS="-I$NCURSES_DIR/include" \
             LDFLAGS="-L$NCURSES_DIR/lib"
 
 make -j$(nproc)
 make install
-
-# Clean up
-cd .. && rm -rf readline-${READLINE_VERSION} readline.tar.gz
 
 # Module
 make_lua_module "readline" "$READLINE_VERSION"
