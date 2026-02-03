9e90e3a415e0d2e7c01e5bd048238e200548ec69
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-03 09:30:57 +0100

    pandoc R

--- r_stack/pandoc.sh
+++ r_stack/pandoc.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 # Download release for linux amd64 from https://github.com/jgm/pandoc/releases
-# unpack and use
+
+# Variables
+TOOL_NAME="pandoc"
+PANDOC_VERSION="3.8.3"
+SOURCE_URL="https://github.com/jgm/pandoc/releases/download/${PANDOC_VERSION}/pandoc-${PANDOC_VERSION}-linux-amd64.tar.gz"
+ARCHIVE_NAME="${TOOL_NAME}_v${PANDOC_VERSION}.tar.gz"
+
+# Paths
+SOURCE_PATH="$INSTALL_DIR/sources/$ARCHIVE_NAME"
+TARGET_DIR="$INSTALL_DIR/$TOOL_NAME/$TOOL_NAME-$PANDOC_VERSION"
+
+# Download to sources directory
+wget -q "$SOURCE_URL" -O "$SOURCE_PATH"
+mkdir -p "$TARGET_DIR"
+tar -xzf "$SOURCE_PATH" -C "$TARGET_DIR" --strip-components=1
+
+# Modules lua file
+make_lua_module "$TOOL_NAME" "$PANDOC_VERSION" "$TARGET_DIR"
---
+++ tools/R/R.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+R_VERSION="4.5.2"
+RDIR=$INSTALL_DIR/R/R-$R_VERSION
+BUILD_DIR=/tmp/R-build
+SRC_DIR=$RDIR/src
+mkdir -p $SRC_DIR
+cd $RDIR
+#wget https://cran.r-project.org/src/base/R-4/R-4.5.2.tar.gz
+tar -xzf R*.tar.gz -C ${SRC_DIR} --strip-components=1
+# Prepare build directory in tmp
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+# Dependencies
+module use $INSTALL_DIR/modulefiles
+module load libcurl/$LIBCURL_VERSION
+module load pcre2/$PCRE2_VERSION
+module load libpng/$LIBPNG_VERSION
+module load libjpeg/$LIBJPEG_VERSION
+module load pandoc/$PANDOC_VERSION
+module load cairo/$CAIRO_VERSION
+module load harfbuzz/$HARFBUZZ_VERSION
+module load fribidi/$FRIBIDI_VERSION
+module load freetype/$FREETYPE_VERSION
+module load bzip2/$BZIP_VERSION
+module load libwebp/$LIBWEBP_VERSION
+module load libvips/$LIBVIPS_VERSION
+
+${SRC_DIR}/configure \
+  --prefix=$RDIR \
+  --enable-memory-profiling \
+  --enable-R-shlib \
+  --with-blas \
+  --with-lapack \
+  --with-libpng \
+  --with-jpeglib \
+  --with-cairo \
+  --with-readline=yes \
+  --with-system-tre=no
+
+make -j$(nproc) && make install
+rm -rf $BUILD
