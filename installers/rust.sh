5dae68ca5601a2ebb0929c98d447aaee554692f6
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-03 17:53:39 +0100

    cm rust

--- installers/rust.sh
+++ installers/rust.sh
@@ -3,13 +3,13 @@
 RUST_VERSION="1.91.1"
 
 # Globals
-export RUSTUP_HOME=$INSTALL_DIR/rust/rust-$RUST_VERSION/rustup
-export CARGO_HOME=$INSTALL_DIR/rust/rust-$RUST_VERSION/cargo
+#export RUSTUP_HOME=$INSTALL_DIR/rust/rust-$RUST_VERSION/rustup
+#export CARGO_HOME=$INSTALL_DIR/rust/rust-$RUST_VERSION/cargo
 
-mkdir -p $RUSTUP_HOME $CARGO_HOME
+#mkdir -p $RUSTUP_HOME $CARGO_HOME
 
-cd $INSTALL_DIR/rust
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf > rustup-init.sh
-chmod +x rustup-init.sh
-./rustup-init.sh --no-modify-path --profile complete
+#cd $INSTALL_DIR/rust
+#curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf > rustup-init.sh
+#chmod +x rustup-init.sh
+#./rustup-init.sh --no-modify-path --profile complete
 
