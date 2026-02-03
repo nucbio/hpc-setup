7a7fa6a462d30587489d5641186c51dc8f976d48
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-03 08:29:32 +0100

    r_dis

--- install.sh
+++ install.sh
@@ -17,10 +17,10 @@ for install_tool in ${REPO_DIR}/installers/*.sh
 
 # Install R_stack (dependencies)
 # loop from r_stack
-for install_tool in ${REPO_DIR}/r_stack/*.sh
-  do 
-    source $install_tool
-  done
+#for install_tool in ${REPO_DIR}/r_stack/*.sh
+#  do 
+#    source $install_tool
+#  done
 
 # Install bioinformatic tools
 for install_tool in ${REPO_DIR}/tools/illumina/*.sh
