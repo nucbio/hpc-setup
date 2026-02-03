927b6ed90b2730e83ed91c36c189ff0939e8639f
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-03 14:25:47 +0100

    ncurses

--- R/R_stack/ncurses.sh
+++ R/R_stack/ncurses.sh
@@ -3,11 +3,13 @@
 TOOL_NAME="ncurses"
 NCURSES_VERSION="6.6" 
 SOURCE_URL="https://ftp.gnu.org/pub/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz"
-TARGET_DIR="$INSTALL_DIR/$TOOL_NAME/$NCURSES_VERSION"
+TARGET_DIR="$INSTALL_DIR/$TOOL_NAME"
 
+mkdir -p $TARGET_DIR
+cd $TARGET_DIR
 # Download and Unpack
-wget -q "$SOURCE_URL" -O ncurses.tar.gz
-tar -xzf ncurses.tar.gz
+wget -q "$SOURCE_URL" -O ncurses-$NCURSES_VERSION.tar.gz
+tar -xzf ncurses-$NCURSES_VERSION.tar.gz
 cd ncurses-${NCURSES_VERSION}
 
 # Configure and Install
@@ -15,13 +17,13 @@ cd ncurses-${NCURSES_VERSION}
             --with-shared \
             --enable-widec \
             --enable-pc-files \
-            --with-pkg-config-libdir="$TARGET_DIR/lib/pkgconfig"
+            --with-pkg-config-libdir="$TARGET_DIR/$TOOL_NAME-$NCURSES_VERSION/lib/pkgconfig"
 
 make -j$(nproc)
 make install
 
 # Clean up
-cd .. && rm -rf ncurses-${NCURSES_VERSION} ncurses.tar.gz
+cd .. && rm -rf ncurses-${NCURSES_VERSION}
 
 # Modules
 make_lua_module "ncurses" "$NCURSES_VERSION"
