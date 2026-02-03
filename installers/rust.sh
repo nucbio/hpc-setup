6a08d426be33925b82c417d8d5cbb0e3386014c3
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-03 18:54:36 +0100

    rust load module

--- installers/rust.sh
+++ installers/rust.sh
@@ -22,8 +22,10 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- \
 
 # TEST: install packs
 # FIX: lock problem
-rustup update
-rustup default stable
+#rustup update
+#rustup default stable
+
+
 
 for install_tool in ${REPO_DIR}/tools/test/*.sh
   do 
--- tools/test/bat.sh
+++ tools/test/bat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+module load rust/$RUST_VERSION
+
 BAT_RELEASE_TAG="${BAT_RELEASE_TAG:-v0.26.1}"
 # Install ripgrep
 cargo install \
