c48e392d6ddbe8bf61fc6069633e046ed97c3a7c
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-03 14:10:27 +0100

    r dep

--- R/R_stack/xz.sh
+++ R/R_stack/xz.sh
@@ -2,7 +2,7 @@
 TOOL_NAME="xz"
 XZ_VERSION="5.8.2"
 SOURCE_URL="https://github.com/tukaani-project/xz/releases/download/v${XZ_VERSION}/xz-${XZ_VERSION}.tar.xz"
-TARGET_DIR="$INSTALL_DIR/xz/$TOOL_VERSION"
+TARGET_DIR="$INSTALL_DIR/xz/$XZ_VERSION"
 
 # Download and Unpack
 wget -q "$SOURCE_URL" -O xz.tar.xz
