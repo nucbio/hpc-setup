b5b46ff8b60d00a3cef90b1017659adff5d3b339
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-04 17:54:26 +0100

    zlib lib

--- modules/templates/zlib.lua
+++ modules/templates/zlib.lua
@@ -15,21 +15,21 @@ whatis("URL: https://zlib.net/")
 local root = "${TOOL_PATH}"
 
 -- Libraries (zlib almost always uses 'lib')
-prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib64"))
-prepend_path("LIBRARY_PATH",    pathJoin(root, "lib64"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib"))
+prepend_path("LIBRARY_PATH",    pathJoin(root, "lib"))
 
 -- Includes
 prepend_path("CPATH",           pathJoin(root, "include"))
-prepend_path("LDFLAGS", "-L" .. pathJoin(root, "lib64"), " ")
+prepend_path("LDFLAGS", "-L" .. pathJoin(root, "lib"), " ")
 prepend_path("CPPFLAGS", "-I" .. pathJoin(root, "include"), " ")
 -- pkg-config
-prepend_path("PKG_CONFIG_PATH", pathJoin(root, "lib64/pkgconfig"))
+prepend_path("PKG_CONFIG_PATH", pathJoin(root, "lib/pkgconfig"))
 
 -- Environment variables for compilers and other tools
 setenv("ZLIB_ROOT", root)
 setenv("ZLIB_HOME", root)
 setenv("ZLIB_INCLUDE_DIR", pathJoin(root, "include"))
-setenv("ZLIB_LIBRARY",     pathJoin(root, "lib64"))
+setenv("ZLIB_LIBRARY",     pathJoin(root, "lib"))
 
 -- Man pages
 prepend_path("MANPATH", pathJoin(root, "share/man"))
