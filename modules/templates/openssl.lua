4cf47343b5d68e14f8841fcf0300de9773a105b0
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-05 05:21:02 +0100

    modules-libs

--- modules/templates/openssl.lua
+++ modules/templates/openssl.lua
@@ -18,25 +18,17 @@ local root = "${TOOL_PATH}"
 -- Binaries (openssl executable)
 prepend_path("PATH", pathJoin(root, "bin"))
 
--- Libraries (Note: OpenSSL 3.x often uses 'lib64', 1.x often uses 'lib')
--- We include both common patterns for safety
-prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib64"))
-prepend_path("LIBRARY_PATH",    pathJoin(root, "lib64"))
-prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib"))
-prepend_path("LIBRARY_PATH",    pathJoin(root, "lib"))
-
 -- Includes
-prepend_path("CPATH",           pathJoin(root, "include"))
+prepend_path("LD_LIBRARY_PATH",  pathJoin(root, "lib64"))
+prepend_path("LIBRARY_PATH",     pathJoin(root, "lib64"))
+prepend_path("CPATH",            pathJoin(root, "include"))
+prepend_path("LDFLAGS", "-L" ..  pathJoin(root, "lib64"), " ")
+prepend_path("CPPFLAGS", "-I" .. pathJoin(root, "include"), " ")
 
 -- pkg-config
 prepend_path("PKG_CONFIG_PATH", pathJoin(root, "lib64/pkgconfig"))
-prepend_path("PKG_CONFIG_PATH", pathJoin(root, "lib/pkgconfig"))
 
 -- Essential variables for Python and CMake builds
 setenv("OPENSSL_ROOT_DIR", root)
 setenv("OPENSSL_INCLUDE_DIR", pathJoin(root, "include"))
 setenv("OPENSSL_LIBRARIES", pathJoin(root, "lib64"))
-
--- For older autotools based configures
-setenv("SSL_CERT_FILE", pathJoin(root, "ssl/cert.pem"))
-setenv("SSL_CERT_DIR",  pathJoin(root, "ssl/certs"))
