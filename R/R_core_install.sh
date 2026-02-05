036d9b2e2d53b179444ae15935d6fe0d07ee8132
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-05 08:16:05 +0100

    freetype

--- R/R_core_install.sh
+++ R/R_core_install.sh
@@ -2,22 +2,22 @@
 
 # Some R dependencies were intalled as core but some are related on meson, which needs python
 # 1. freetype (use zlib, bzip2, png, harfbuzz)
-source $REPO_DIR/core/freetype.sh
+source $REPO_DIR/R/R_core/freetype.sh
 
 # 2. fribidi (load freetype)
-module load freetype/$FREETYPE_VERSION
-source $REPO_DIR/R/R_core/fribidi.sh
+#module load freetype/$FREETYPE_VERSION
+#source $REPO_DIR/R/R_core/fribidi.sh
 # 3. harfbuzz
-module load fribidi/$FRIBIDI_VERSION
-source $REPO_DIR/R/R_core/harfbuzz.sh
+#module load fribidi/$FRIBIDI_VERSION
+#source $REPO_DIR/R/R_core/harfbuzz.sh
 
 # 4. cairo (load freetype, fribidi, harfbuzz)
-module load harfbuzz/$HARFBUZZ_VERSION
-source $REPO_DIR/R/R_core/cairo.sh
+#module load harfbuzz/$HARFBUZZ_VERSION
+#source $REPO_DIR/R/R_core/cairo.sh
 
 # 5. libvips (load libjpeg, libpng, libwebp, Cairo)
-module load libjpeg/$LIBJPEG_VERSION
-module load libpng/$LIBPNG_VERSION
-module load cairo/$CAIRO_VERSION
-source $REPO_DIR/R/R_core/libvips.sh
+#module load libjpeg/$LIBJPEG_VERSION
+#module load libpng/$LIBPNG_VERSION
+#module load cairo/$CAIRO_VERSION
+#source $REPO_DIR/R/R_core/libvips.sh
 
