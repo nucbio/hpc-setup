dd2e440bb32d6cf6e3658e68652395bc997d3fa3
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-06 20:30:07 +0100

    hdf5 alternative configure

--- tools/nanopore/hdf5.sh
+++ tools/nanopore/hdf5.sh
@@ -12,7 +12,18 @@ wget https://support.hdfgroup.org/releases/hdf5/v1_14/v1_14_6/downloads/hdf5-$HD
 tar -xzf "hdf5-${HDF5_VERSION}.tar.gz"
 cd "hdf5-${HDF5_VERSION}"
 ./configure --prefix="$INSTALL_DIR" --enable-build-mode=production
-make # parallel may cause error
+
+# Alternative configure
+# ./configure \
+#     --prefix="$INSTALL_DIR" \
+#     --enable-build-mode=production \
+#     --enable-threadsafe \
+#     --enable-unsupported \
+#     --with-pic \
+#     --enable-shared \
+#     --enable-cxx
+
+make # -j may cause error
 make install
 
 cd /
