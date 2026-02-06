6bd0ea045246951503441baab575b0df074bf258
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-06 20:25:36 +0100

    hdf5 no j

--- tools/nanopore/hdf5.sh
+++ tools/nanopore/hdf5.sh
@@ -12,7 +12,7 @@ wget https://support.hdfgroup.org/releases/hdf5/v1_14/v1_14_6/downloads/hdf5-$HD
 tar -xzf "hdf5-${HDF5_VERSION}.tar.gz"
 cd "hdf5-${HDF5_VERSION}"
 ./configure --prefix="$INSTALL_DIR" --enable-build-mode=production
-make -j$(nproc)
+make # parallel may cause error
 make install
 
 cd /
