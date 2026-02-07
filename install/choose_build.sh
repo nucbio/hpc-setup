351443b9daddae80ef0e4dfa337004afedf479ec
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-07 12:32:21 +0100

    choose_build

--- install/choose_build.sh
+++ install/choose_build.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-echo "Select where to compile tools."
-echo "Using '/tmp' can be faster and is recommended on HPC systems."
-echo "Using '$PWD' will keep entire set-up within a single directory."
-echo "[1] '$PWD'"
-echo "[2] '/tmp';"
+if [[ -t 0 ]]; then
+    echo "Select where to compile tools."
+    echo "Using '/tmp' can be faster and is recommended on HPC systems."
+    echo "Using '$INSTALL_DIR' will keep entire set-up within a single directory."
+    echo "[1] '/tmp'"
+    echo "[2] '$INSTALL_DIR';"
 
-while true; do
-    read -p "Press 1 or 2 to chose a build directory: " opt
-    case $opt in
-        1) export USE_TMP=0; break;;
-        2) export USE_TMP=1; break;;
-        *) echo "Please enter 1 or 2.";;
-    esac
-done
+    while true; do
+        read -p "Press 1 or 2 to chose a build directory: " opt
+        case $opt in
+            1) export USE_TMP=0; break;;
+            2) export USE_TMP=1; break;;
+            *) echo "Please enter 1 or 2.";;
+        esac
+    done
 
-echo "Selected option: $USE_TMP"
+    echo "Selected option: $USE_TMP"
+else
+    USE_TMP=1
+fi
+
+
