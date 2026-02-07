4df10f99210a8a68d30aedb53065d384f0733fc0
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-07 01:50:14 +0100

    hdf5 re

--- tools/nanopore/hdf5.sh
+++ tools/nanopore/hdf5.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 # Configuration
+TOOL_NAME="hdf5"
 export HDF5_VERSION="1.14.6"
+SOURCE_URL="https://support.hdfgroup.org/releases/$TOOL_NAME/v1_14/v1_14_6/downloads/$TOOL_NAME-$HDF5_VERSION.tar.gz"
 
+TARGET_DIR="$INSTALL_DIR/$TOOL_NAME/$TOOL_NAME-$HDF5_VERSION"
+ARCHIVE="$TOOL_NAME-$HDF5_VERSION.tar.gz"
 BUILD_DIR="/tmp/hdf5-build"
 
 # Create directories
-mkdir -p "$BUILD_DIR"
-cd "$BUILD_DIR"
-wget https://support.hdfgroup.org/releases/hdf5/v1_14/v1_14_6/downloads/hdf5-$HDF5_VERSION.tar.gz
-tar -xzf "hdf5-${HDF5_VERSION}.tar.gz"
-cd "hdf5-${HDF5_VERSION}"
-#./configure --prefix="$INSTALL_DIR" --enable-build-mode=production
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+wget -qL $SOURCE_URL -O $ARCHIVE
+tar -xzf $ARCHIVE
+cd "$TOOL_NAME-$HDF5_VERSION"
+./configure --prefix=$TARGET_DIR --enable-build-mode=production
 
 # Alternative configure
-./configure \
-    --prefix="$INSTALL_DIR" \
-    --enable-build-mode=production \
-    --enable-threadsafe \
-    --enable-unsupported \
-    --enable-shared \
-    --enable-cxx \
-    --with-pic \
-    --with-zlib
+# ./configure \
+#     --prefix="$TARGET_DIR" \
+#     --enable-build-mode=production \
+#     --enable-threadsafe \
+#     --enable-unsupported \
+#     --enable-shared \
+#     --enable-cxx \
+#     --with-pic \
+#     --with-zlib
 
-make # -j may cause error
+make -j 4
 make install
 
 cd $REPO_DIR
-rm -rf "$BUILD_DIR"
+# rm -rf "$BUILD_DIR"
+
+# Module
+make_lua_module "$TOOL_NAME" "$HDF5_VERSION"
