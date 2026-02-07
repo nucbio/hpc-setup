0db32c64ed929f0cb5ebecf8f552d1449ef702c2
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-07 03:38:57 +0100

    dialog; hdf5 x

--- modules/templates/pcre2.lua
+++ modules/templates/pcre2.lua
@@ -7,12 +7,12 @@ prepend_path("PATH", pathJoin(root, "bin"))
 
 -- Public include directory
 prepend_path("CPATH", pathJoin(root, "include"))
-setenv("CPPFLAGS", "-I" .. pathJoin(root, "include"))
+prepend_path("CPPFLAGS", "-I" .. pathJoin(root, "include"), " ")
 
 -- Library paths
 prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib64"))
 prepend_path("LIBRARY_PATH",    pathJoin(root, "lib64"))
-setenv("LDFLAGS", "-L" .. pathJoin(root, "lib64"))
+prepend_path("LDFLAGS", "-L" .. pathJoin(root, "lib64"), " ")
 
 -- pkg-config
 prepend_path("PKG_CONFIG_PATH", pathJoin(root, "lib64/pkgconfig"))
