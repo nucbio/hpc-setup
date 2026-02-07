6d48d4c7a1c774144b0711560fa39d580949c24e
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-07 12:59:01 +0100

    gum

--- core/gum.sh
+++ core/gum.sh
@@ -11,10 +11,10 @@ cd /tmp
 TARBALL="gum_${GUM_VERSION}_Linux_${ARCH}.tar.gz"
 URL="https://github.com/charmbracelet/gum/releases/download/v0.17.0/gum_0.17.0_Linux_x86_64.tar.gz"
 
-wget -L "$URL" -O gum.tar.gz
+wget "$URL" -O gum.tar.gz
 
-tar -xzf gum.tar.gz gum
-
+tar -xzf gum.tar.gz
+cd gum
 install -m 0755 gum "$GUM_BIN"
 #rm -f gum gum.tar.gz
 
