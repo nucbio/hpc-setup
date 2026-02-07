0dc6eb7a8c1d27031037ae16fa427b10f0fc8c5c
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-07 11:11:40 +0100

    test essential

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ set -e
 
 # Get Functions
 source $REPO_DIR/install/helpers.sh
-source $REPO_DIR/install/check_modules
+source $REPO_DIR/install/check_essential.sh
+source $REPO_DIR/modules/check_modules.sh
 # source ${REPO_DIR}/install/make_lua_module.sh
 #
 # # Set locations
