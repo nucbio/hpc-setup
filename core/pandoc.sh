0e20e4f57180fef3fad9aa6f9362f761dd6c9f1f
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-08 12:46:39 +0100

    pandoc dirs

--- core/pandoc.sh
+++ core/pandoc.sh
@@ -4,14 +4,14 @@ PKG_NAME="pandoc"
 PKG_VERSION="3.8.3"
 export PANDOC_VERSION=$PKG_VERSION
 PKG_SRC_URL="https://github.com/jgm/pandoc/releases/download/${PANDOC_VERSION}/pandoc-${PANDOC_VERSION}-linux-amd64.tar.gz"
-PKG_SRC_DIR="$SOURCES_DIR/${PKG_NAME}_v${PANDOC_VERSION}.tar.gz"
+PKG_ARCHIVE="$SOURCES_DIR/$PKG_NAME-$PKG_VERSION.tar.xz"
 
 # Set PKG_SRC_DIR, PKG_PREFIX
 set_pkg_dirs  $PKG_NAME $PKG_VERSION
 
 # Download to sources directory
-wget -nv "$PKG_SRC_URL" -O "$PKG_SRC_DIR"
-tar -xzf "$PKG_SRC_DIR" -C "$PKG_PREFIX" --strip-components=1
+wget -nv "$PKG_SRC_URL" -O "$PKG_ARCHIVE"
+tar -xzf "$PKG_ARCHIVE" -C "$PKG_PREFIX" --strip-components=1
 
 # Modules lua file
 make_lua_module "$PKG_NAME" "$PANDOC_VERSION" "$PKG_PREFIX"
