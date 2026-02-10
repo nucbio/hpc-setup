6dc5818688a6eca3bb18f62d27ee44942688b456
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-10 14:47:41 +0100

    pkg_install

--- core/cairo.sh
+++ core/cairo.sh
@@ -4,6 +4,6 @@ echo "Install cairo"
 
 pkg_install -n "cairo" \
             -v "1.18.4" \
-            -u "https://cairographics.org/releases/cairo-${CAIRO_VERSION}.tar.xz" \
+            -u "https://cairographics.org/releases/cairo-1.18.4.tar.xz" \
             -t "meson" \
             -o "--buildtype=release -Dtests=disabled"
