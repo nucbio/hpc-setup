0dffc01f365fa42e1effc04e37777caea7ff9050
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-11 12:12:45 +0100

    freetype deps

--- core/freetype.sh
+++ core/freetype.sh
@@ -2,6 +2,14 @@
 
 echo "Install freetype"
 
+# Load modules
+module use $INSTALL_DIR/modulefiles
+module load zlib
+module load bzip2
+module load libpng
+module load harfbuzz
+
+# Standard meson installation
 pkg_install -n "freetype" \
             -v "2.14.1" \
             -u "https://download.savannah.gnu.org/releases/freetype/freetype-2.14.1.tar.gz" \
