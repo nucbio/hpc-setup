31151e3b4c540707574e2bf215223c034c27275a
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-12 08:48:21 +0100

    fontconfig

--- core/fontconfig.sh
+++ core/fontconfig.sh
@@ -10,5 +10,5 @@ export FONTCONFIG_VERSION="2.17.1"
 
 pkg_install \
     -n "fontconfig" \
-    -v "$FONTFONFIG_VERSION" \
-    -u "https://gitlab.freedesktop.org/api/v4/projects/890/packages/generic/fontconfig/$FONTFONFIG_VERSION/fontconfig-$FONTFONFIG_VERSION.tar.xz"
+    -v "$FONTCONFIG_VERSION" \
+    -u "https://gitlab.freedesktop.org/api/v4/projects/890/packages/generic/fontconfig/$FONTCONFIG_VERSION/fontconfig-$FONTCONFIG_VERSION.tar.xz"
