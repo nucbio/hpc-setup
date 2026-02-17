82ef700e772ccc570c96a0f58489a1bc695f45e2
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-17 22:22:05 +0100

    rm flag

--- functions/pkg_install.sh
+++ functions/pkg_install.sh
@@ -7,7 +7,7 @@ pkg_install() {
     local OPTIND=1
     
     local PKG_NAME PKG_VERSION PKG_URL PKG_TYPE="conf" EXTRA_OPTS=""
-    echo "FLAG1"
+    
     # Parse Arguments
     # n: Name, v: Version, u: URL, t: Type, o: Options (the string you requested)
     while getopts "n:v:u:t:o:" opt; do
