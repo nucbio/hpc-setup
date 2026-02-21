8cc95b83a3941cdcc265c8214bec25caf73c1bc3
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-21 10:09:26 +0100

    libxml2 with-zlib

--- core/libxml2.sh
+++ core/libxml2.sh
@@ -4,8 +4,12 @@ echo "Install libxml2"
 
 export LIBXML2_VERSION="2.15.1"
 
+# for R xml support
+ZLIB="$INSTALL_DIR/zlib/zlib-$ZLIB_VERSION"
+
 pkg_install \
     -n "libxml2" \
     -v "$LIBXML2_VERSION" \
     -u "https://download.gnome.org/sources/libxml2/2.15/libxml2-$LIBXML2_VERSION.tar.xz" \
+    -o "--with-zlib $ZLIB"
 
