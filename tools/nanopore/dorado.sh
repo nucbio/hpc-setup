00447e1ca167763a898f112af029a7035e84b300
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-25 18:06:02 +0100

    dorado

--- tools/nanopore/dorado.sh
+++ tools/nanopore/dorado.sh
@@ -11,6 +11,21 @@ pkg_intall \
   -u "https://cdn.oxfordnanoportal.com/software/analysis/dorado-${PKG_VERSION}-linux-x64.tar.gz" \
   -t "unpack"
 
+# Download selected Dorado models
+
+DORADO_MODELS="$INSTALL_DIR/dorado/models"
+
+mkdir -p $DORADO_MODELS
+
+module load dorado
+
+dorado download \
+    --directory $DORADO_MODELS \
+    --model dna_r10.4.1_e8.2_400bps_sup@v4.3.0 \
+    --model dna_r10.4.1_e8.2_400bps_sup@v5.0.0 \
+    --model dna_r10.4.1_e8.2_400bps_sup@v5.0.0_6mA@v3 \
+    --model dna_r10.4.1_e8.2_400bps_sup@v5.0.0_5mCG_5hmCG@v3
+
 # PKG_NAME="dorado"
 # export DORADO_VERSION="1.3.0"
 #
