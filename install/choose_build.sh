64e7a591aab6dc4cd115c6fae3dbaf4bb26279cd
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-26 10:12:38 +0100

    up

--- install/choose_build.sh
+++ install/choose_build.sh
@@ -5,10 +5,12 @@ GUM="$INSTALL_DIR/gum/gum-$GUM_VERSION/gum"
 
 echo "-----------------------------------------------"
 echo "Select where to compile tools:"
-echo "• /tmp is faster (recommended for HPC)"
-echo "• $INSTALL_DIR keeps everything in one place"
+#echo "/tmp is faster (recommended for HPC)"
+#echo "• $INSTALL_DIR keeps everything in one place"
 
-CHOICE=$("$GUM" choose "/tmp" "$INSTALL_DIR")
+CHOICE=$("$GUM" choose \
+  "• /tmp (is faster - recommended for HPC)" \
+  "• $INSTALL_DIR (keeps everything in one place)")
 
 if [ "$CHOICE" = "/tmp" ]; then
     export USE_TMP=1
@@ -19,5 +21,5 @@ else
 fi
 
 echo "Selected build directory is : $CHOICE"
-sleep 2
+sleep 3
 
