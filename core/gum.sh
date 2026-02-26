def8faddd34431173e7f62852bf78ef8bbbfd100
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-26 09:17:10 +0100

    gum

--- core/gum.sh
+++ core/gum.sh
@@ -7,7 +7,7 @@ export GUM_VERSION="0.17.0"
 pkg_install \
   -n "gum" \
   -v "$GUM_VERSION" \
-  -u "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_Linux_x86_64.tar.gz"
+  -u "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_Linux_x86_64.tar.gz" \
   -t "unpack"
 
 ###############################################################################
