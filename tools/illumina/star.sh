11d07b52294004f19ff9c865d132b6cecd4bfe54
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-28 23:26:34 +0100

    star unpack

--- tools/illumina/star.sh
+++ tools/illumina/star.sh
@@ -5,7 +5,7 @@ echo "Installing STAR aligner"
 pkg_install \
   -n "star" \
   -v "2.7.11b" \
-  -u "https://github.com/alexdobin/STAR/releases/download/$PKG_VERSION/STAR_$PKG_VERSION.zip" \
+  -u "https://github.com/alexdobin/STAR/releases/download/2.7.11b/STAR_2.7.11b.zip" \
   -t "unpack"
 
 
