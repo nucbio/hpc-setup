bb571bec14256e75afb43c07ad949aef97a919ea
Mark Boltengagen <m.boltengagen@gmail.com>
2026-02-28 12:23:19 +0100

    dorado models

--- tools/nanopore/dorado.sh
+++ tools/nanopore/dorado.sh
@@ -22,10 +22,10 @@ mkdir -p $DORADO_MODELS
 module load dorado
 
 for MODEL in \
-  --model dna_r10.4.1_e8.2_400bps_sup@v4.3.0 \
-  --model dna_r10.4.1_e8.2_400bps_sup@v5.0.0 \
-  --model dna_r10.4.1_e8.2_400bps_sup@v5.0.0_6mA@v3 \
-  --model dna_r10.4.1_e8.2_400bps_sup@v5.0.0_5mCG_5hmCG@v3
+  dna_r10.4.1_e8.2_400bps_sup@v4.3.0 \
+  dna_r10.4.1_e8.2_400bps_sup@v5.0.0 \
+  dna_r10.4.1_e8.2_400bps_sup@v5.0.0_6mA@v3 \
+  dna_r10.4.1_e8.2_400bps_sup@v5.0.0_5mCG_5hmCG@v3
 do
   dorado download \
     --model-directory "$DORADO_MODELS" \
