0e6bed9c3683d0f71c8b573d7263ecae86c02d25
Mark Boltengagen <m.boltengagen@gmail.com>
2026-03-04 21:08:52 +0100

    git i

---
+++ modules/templates/git.lua
@@ -0,0 +1,14 @@
+-- ${TOOL} version ${TOOL_VERSION}
+
+whatis("$TOOL version $TOOL_VERSION")
+
+-- Using the resolved TOOL_PATH from the Bash function
+prepend_path("PATH", "$TOOL_PATH")
+
+load("libcurl/${LIBCURL_VERSION}")
+load("openssl/${OPENSSL_VERSION}")
+
+-- Message
+if mode() == "load" then
+    LmodMessage("Loading $TOOL $TOOL_VERSION.")
+end
