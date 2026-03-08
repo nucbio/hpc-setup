7f937c32fdf4924832710ca36d21ebd20652f2a4
Mark Boltengagen <m.boltengagen@gmail.com>
2026-03-08 15:25:47 +0100

    jq

---
+++ core/jq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PKG_NAME="jq"
+export JQ_VERSION=1.8.1
+
+JQ_DIR=$INSTALL_DIR/$PKG_NAME/$PKG_NAME-$JQ_VERSION
+
+mkdir -p $JQ_DIR
+cd $JQ_DIR
+wget -O jq https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-linux64 && chmod +x jq
+
+# Modules lua file
+make_lua_module $PKG_NAME $JQ_VERSION
