522e6f04eb2cbb69d4fcdb559a6be387d907ee86
Mark Boltengagen <m.boltengagen@gmail.com>
2026-03-27 12:00:21 +0100

    glib 2.88.0

--- core/glib.sh
+++ core/glib.sh
@@ -8,9 +8,9 @@ module load pcre2
 module load libffi
 module load gettext
 
-export GLIB_VERSION="2.87.2"
+export GLIB_VERSION="2.88.0"
 
 pkg_install -n "glib" \
             -v "$GLIB_VERSION" \
-            -u "https://download.gnome.org/sources/glib/2.87/glib-2.87.2.tar.xz" \
+            -u "https://download.gnome.org/sources/glib/2.88/glib-2.88.0.tar.xz" \
             -t "meson"
